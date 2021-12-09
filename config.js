2fd3cefb342065510d8171331e7e3d7affc502ec
namvirus <95855416+namvirus@users.noreply.github.com>
2021-12-09 18:42:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay a chở bà xã về nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100013455458557' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100013455458557' 
 }
